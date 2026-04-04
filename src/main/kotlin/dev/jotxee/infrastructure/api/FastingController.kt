7ac21cb18a4421824ba1f1724b85685fedac504e
Jose <joselitosbd@gmail.com>
2026-04-04 13:52:53 +0200

    refactor: update FastingController to use FastingDto and rename fasting.yml to fastingDto.yml

--- src/main/kotlin/dev/jotxee/infrastructure/api/FastingController.kt
+++ src/main/kotlin/dev/jotxee/infrastructure/api/FastingController.kt
@@ -1,19 +1,19 @@
 package dev.jotxee.infrastructure.api
 
 import dev.jotxee.api.FastingApi
-import dev.jotxee.model.Fasting
+import dev.jotxee.model.FastingDto
 import io.micronaut.http.annotation.Controller
 import java.time.ZonedDateTime
 
 @Controller
 class FastingController : FastingApi {
 
-    override fun getFasting(): List<Fasting> {
+    override fun getFasting(): List<FastingDto> {
         return listOf(
-            Fasting("Intermittent Fasting 16/8", ZonedDateTime.parse("2026-04-03T20:00:00Z"))
+            FastingDto("Intermittent Fasting 16/8", ZonedDateTime.parse("2026-04-03T20:00:00Z"))
                 .end(ZonedDateTime.parse("2026-04-04T12:00:00Z"))
                 .notes("Mock data"),
-            Fasting("OMAD", ZonedDateTime.parse("2026-04-02T18:00:00Z"))
+            FastingDto("OMAD", ZonedDateTime.parse("2026-04-02T18:00:00Z"))
                 .end(ZonedDateTime.parse("2026-04-03T18:00:00Z"))
                 .notes("One meal a day")
         )
